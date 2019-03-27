c18243942c54929f5407bf1eb80f28ec2695a565
sauravka <sauravka@amazon.com>
2019-03-27 11:28:25 -0700

    Updated html,css,js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,44 @@
 "use strict";
 
+//Storing all the questions on Array
 var questionArray = ["Does my name contains letter 'g'?(y/n)", 
 "My favourite basketball team is Celtics?(y/n)", "I am 23 years old ?(y/n)",
 "I prefer dogs over cats(y/n)?", "I love snow (y/n)? "];
-
-var answers = ["My names doesn't contain letter 'g'","My favourite team is Celtics","I am not that young","I prefer dogs", "Don't like snow at all!"]
-
-prompt("Welcome! Question after this would require 'y'/'n' or 'yes'/'no' as response ");
+//Correct Answers
+var answers = ["My names doesn't contain letter 'g'","My favourite team is Celtics","I am 27","I prefer dogs", "Don't like snow at all!"]
+//Welcome Screen
+alert("Welcome!\nPlease respond following question with 'y'/'n' or 'yes'/'no' as response ");
 var i = 0;
 var response = [];
-
-for(i; i<questionArray.length-1;i++)
+function responseCheck(response){
+  if(i%2==0){
+    if(response=="y"||response=="yes"){
+        alert("Sorry! Incorrect!!");
+    }
+    else{
+        alert("Correct");
+    }
+  }
+  else{
+    if(response=="y"||response=="yes"){
+      alert("Correct!");
+    }
+    else{
+      alert("Incorrect!!");
+    } 
+  }
+}
+//Show prompt until user enter correct answers
+for(i; i<questionArray.length;i++)
 {
  do{
   response = prompt(questionArray[i]);
   response = response.toLowerCase();
+  if(response!="y"&&response!="n"&&response!="yes"&&response!="no"){
+    alert("Plese enter either 'y/n' or 'yes/no' as response");
+  }
   }while(response!="y"&&response!="n"&&response!="yes"&&response!="no");
   console.log("For"+ questionArray[i] + " User responded: " + response);
-  alert("Correct Answer:" + answers[i]);
+  var temp = responseCheck(response);
 }
 
